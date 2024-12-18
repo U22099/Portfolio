9fb678cee66a6102d8e24bef5b3e2549b7e89619
Daniel <156960927+U22099@users.noreply.github.com>
2024-12-18 21:17:20 +0000

    update

--- src/components/app/navigation.jsx
+++ src/components/app/navigation.jsx
@@ -12,20 +12,20 @@ export function Navigation(){
     setNav(page);
   }
  return(
-   <motion.nav initial={{x: -300, opacity: 0.3}} animate={{x: 0, opacity: 1}} transition={{duration: 1}} ariaLabel="Navigation Menu" role="navigation" className="bg-[hsla(262.1,83.3%,57.8%,0.5)] dark:bg-[hsla(263.4,70%,50.4%,0.5)] p-3 rounded-tr-xl rounded-br-xl fixed left-0 bottom-40 gap-5 flex flex-col">
-    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 0.5 }} role="button" onClick={() => navigate("home")}>
+   <motion.nav initial={{x: -300, opacity: 0}} animate={{x: 0, opacity: 1}} transition={{duration: 1}} ariaLabel="Navigation Menu" role="navigation" className="bg-[hsla(262.1,83.3%,57.8%,0.5)] dark:bg-[hsla(263.4,70%,50.4%,0.5)] p-3 rounded-tr-xl rounded-br-xl fixed left-0 bottom-40 gap-5 flex flex-col">
+    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 0.5, tween: 1 }} role="button" onClick={() => navigate("home")}>
       <GoHomeFill className={ (nav === "home" ? "fill-black dark:fill-white":"fill-muted-foreground") + " w-8 h-8"}/>
     </motion.a>
-    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 0.7 }} role="button" onClick={() => navigate("about")}>
+    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 0.7, tween: 1  }} role="button" onClick={() => navigate("about")}>
       <FaUser className={ (nav === "about" ? "fill-black dark:fill-white":"fill-muted-foreground") + " w-8 h-8"}/>
     </motion.a>
-    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 0.9 }} role="button" onClick={() => navigate("skills")}>
+    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 0.9, tween: 1  }} role="button" onClick={() => navigate("skills")}>
       <VscVscode className={ (nav === "skills" ? "fill-black dark:fill-white":"fill-muted-foreground") + " w-8 h-8"}/>
     </motion.a>
-    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 1.1 }} role="button" onClick={() => navigate("projects")}>
+    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 1.1, tween: 1  }} role="button" onClick={() => navigate("projects")}>
       <FaCode className={ (nav === "projects" ? "fill-black dark:fill-white":"fill-muted-foreground") + " w-8 h-8"}/>
     </motion.a>
-    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 1.3 }} role="button" onClick={() => navigate("contact")}>
+    <motion.a initial={{x: -100, y: 100, opacity: 0.3}} animate={{x: 0, y: 0, opacity: 1}} transition={{ delay: 1.3, tween: 1  }} role="button" onClick={() => navigate("contact")}>
       <MdAlternateEmail className={ (nav === "contact" ? "fill-black dark:fill-white":"fill-muted-foreground") + " w-8 h-8"}/>
     </motion.a>
    </motion.nav>
