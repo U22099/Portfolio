e9757d45f46445a727764c13978c624ab817b96c
U22099 <nifemiolaniyi4@gmail.com>
2024-12-19 10:12:51 +0100

    update home.jsx

--- src/components/app/home.jsx
+++ src/components/app/home.jsx
@@ -1,5 +1,13 @@
 export function Home(){
   return(
-    <p>Home</p> 
+    <main className="ml-20 flex flex-col gap-2">
+      <p className="text-primary font-bold text-md">Hey there 😉</p>
+      <div className="flex flex-col gap-1">
+        <p className="text-muted-foreground text-sm">What do you think?</p>
+        <p className="text-muted-foreground text-sm">Lame right? 😂</p>
+      </div>
+      <h1 className="text-lg text-primary font-bold">D name's Daniel</h1>
+      <h1 className="text-md text-accent-foreground font-bold">Welcome to the funniest portfolio you've ever seen</h1>
+    </main> 
   )
 }
