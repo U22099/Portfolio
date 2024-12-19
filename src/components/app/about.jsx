c324b062148f281318fd971b7d2445a51c96343f
U22099 <nifemiolaniyi4@gmail.com>
2024-12-19 11:25:07 +0100

    update about.jsx

--- src/components/app/about.jsx
+++ src/components/app/about.jsx
@@ -1,5 +1,10 @@
 export function About() {
   return (
-    <p>About</p>
+    <main>
+      <p>Ask one of my fellow devs to describe me and you might get...😂</p>
+      <p>Daniel: 17, MERN + Next.js wizard, crafting digital magic one line of code at a time. (May or may not have a secret lair filled with glowing keyboards.)</p>
+      <p>Ok lets get serious..</p>
+      <p>I’m Daniel, a 17-year-old full-stack developer proficient in the MERN stack and Next.js. While my coding journey spans just eight months, I’ve focused intensely on building a diverse portfolio of projects. These range from AI-powered applications like chatbots and content summarizers to complex systems such as real-time chat and music streaming platforms. My rapid progress reflects a dedication to continuous learning and a passion for crafting innovative, user-focused solutions</p>
+    </main>
   )
 }
