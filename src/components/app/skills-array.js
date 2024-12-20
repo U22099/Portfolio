0b87fcb1e6e3f7c36c88468fe004f210cee88fc7
U22099 <nifemiolaniyi4@gmail.com>
2024-12-20 13:51:01 +0100

    update

---
+++ src/components/app/skills-array.js
@@ -0,0 +1,64 @@
+export const frontendSkills = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "TypeScript",
+  "SASS/SCSS",
+  "Responsive Design",
+  "Tailwind CSS",
+  "Shadcn UI",
+  "Web Accessibility",
+  "Progressive Web Apps (PWA)",
+  "Webpack",
+  "ES6+",
+  "Cross-Browser Compatibility"
+];
+
+export const backendSkills = [
+  "Node.js",
+  "Express.js",
+  "GraphQL",
+  "REST APIs",
+  "WebSockets",
+  "Authentication (JWT, OAuth)",
+  "Server-Side Rendering (SSR)",
+];
+
+export const database = [
+  "PostgreSQL",
+  "MongoDB",
+  "Firebase",
+  "NoSQL",
+  "Database Design",
+  "ORM (Sequelize, Mongoose, TypeORM)",
+  "Database Optimization",
+];
+
+export const versionControl = [
+  "Git",
+  "GitHub",
+  "Branching",
+  "Pull Requests", 
+  "Merging",
+  "Versioning",
+]
+
+export const cicd = [
+  "GitHub Actions",
+  "Render",
+  "Vercel",
+  "GitHub Pages",
+  "Netlify"
+]
+
+export const softSkills = [
+  "Problem-Solving",
+  "Team Collaboration",
+  "Communication Skills",
+  "Time Management",
+  "Adaptability",
+  "Leadership",
+  "Critical Thinking",
+  "Creativity"
+]
--- src/components/app/skills.jsx
+++ src/components/app/skills.jsx
@@ -4,25 +4,44 @@ import {
   CardContent
 } from "@/components/ui/card";
 import { FaReact } from "react-icons/fa";
+import { frontendSkills, backendSkills, database, cicd, versionControl, softSkills } from "./skills-array";
 
 export function Skills(){
   return(
     <main className="max-h-[80vh] overflow-y-scroll scrollbar flex flex-col gap-2">
       <motion.h1 initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{ duration: 1.5, delay:0 }} className="text-primary font-bold text-md underline">Frontend</motion.h1>
       <section>
-        <SkillCard>
-         <FaReact className="w-8 h-8"/>
-        </SkillCard>
+        {frontendSkills.map((name, i) => <SkillCard name={name} key={i} />)}
+      </section>
+      <motion.h1 initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{ duration: 1.5, delay:0 }} className="text-primary font-bold text-md underline">Backend</motion.h1>
+      <section>
+        {backendSkills.map((name, i) => <SkillCard name={name} key={i} />)}
+      </section>
+      <motion.h1 initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{ duration: 1.5, delay:0 }} className="text-primary font-bold text-md underline">Database</motion.h1>
+      <section>
+        {database.map((name, i) => <SkillCard name={name} key={i} />)}
+      </section>
+      <motion.h1 initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{ duration: 1.5, delay:0 }} className="text-primary font-bold text-md underline">Version Control</motion.h1>
+      <section>
+        {versionControl.map((name, i) => <SkillCard name={name} key={i} />)}
+      </section>
+      <motion.h1 initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{ duration: 1.5, delay:0 }} className="text-primary font-bold text-md underline">CI/CD</motion.h1>
+      <section>
+        {cicd.map((name, i) => <SkillCard name={name} key={i} />)}
+      </section>
+      <motion.h1 initial={{opacity: 0, y: 100}} animate={{opacity: 1, y: 0}} transition={{ duration: 1.5, delay:0 }} className="text-primary font-bold text-md underline">Other Skills</motion.h1>
+      <section>
+        {softSkills.map((name, i) => <SkillCard name={name} key={i} />)}
       </section>
     </main>
   )
 }
 
-function SkillCard({ children }){
+function SkillCard({ name }){
   return(
     <Card className="flex justify-center items-center m-0 w-fit h-fit">
       <CardContent className="flex justify-center items-center text-center m-0">
-        {children}
+        <p className="font-bold text-sm">• {name}</p>
       </CardContent>
     </Card>
   )
