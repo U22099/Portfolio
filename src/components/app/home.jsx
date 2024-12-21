e42f3f9b24a0b5a4f361d51f6ceb6ea026e2c0cb
U22099 <nifemiolaniyi4@gmail.com>
2024-12-21 21:12:32 +0100

    update

--- src/components/app/home.jsx
+++ src/components/app/home.jsx
@@ -6,7 +6,7 @@ export function Home(){
   const  setNav = useNav(state => state.setNav);
   return(
     <main className="flex flex-col gap-2">
-      <motion.p initial={{opacity: 0, h: "0%"}} animate={{opacity: 1, h: "100%"}} transition={{ duration: 1.5 }} className="text-gradient font-bold text-3xl">Hey there <span className="text-primary">😉</span></motion.p>
+      <motion.p initial={{opacity: 0, h: "0%"}} animate={{opacity: 1, h: "100%"}} transition={{ duration: 1.5 }} className="text-gradient font-bold text-3xl">Hey there <span className="text-white">😉</span></motion.p>
       <motion.div initial={{x: 700}} animate={{x: 0}}  className="flex flex-col gap-1">
         <p className="text-muted-foreground text-xs font-bold font-mono animate-pulse">(new website()).runPortfolio(&apos;Daniel&apos;</p>
         <p className="text-muted-foreground text-xs font-bold font-mono">{"</>"}</p>
@@ -14,7 +14,7 @@ export function Home(){
       <motion.h1 initial={{x: -100}} animate={{x: 0}}  className="text-4xl font-bold font-mono animate-bounce">My name's <span className="text-gradient">Daniel</span></motion.h1>
       <motion.h1 initial={{x: 100 }} animate={{x: 0}} className="text-lg text-accent-foreground font-bold">• Fullstack web developer</motion.h1>
       <motion.h1 initial={{y: 150, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{ duration: 1.1 }} className="text-md text-muted-foreground font-bold">Welcome to the simplest portfolio you've ever seen</motion.h1>
-      <Button variant="outline" className="backdrop-blur-md w-fit" onClick={() => setNav("contact")}>Contact</Button>
+      <Button variant="outline" className="backdrop-blur-md w-fit bg-gradient-to-r from-primary to-fuchsia-500" onClick={() => setNav("contact")}>Contact</Button>
     </main> 
   )
 }
