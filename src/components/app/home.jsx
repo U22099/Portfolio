0d715bca8e83b275c5ebe1fd4700e0713889cf24
U22099 <nifemiolaniyi4@gmail.com>
2024-12-22 16:00:39 +0100

    update home.jsx

--- src/components/app/home.jsx
+++ src/components/app/home.jsx
@@ -5,7 +5,7 @@ import { useNav, useAnimation } from "@/store";
 
 export function Home(){
   const setNav = useNav(state => state.setNav);
-  const setAnimation = useAnimation(state => state.setAnimation);
+  const { animation, setAnimation } = useAnimation();
   return(
     <main aria-label="Homepage" className="flex flex-col gap-2">
       <motion.p initial={{opacity: 0, h: "0%"}} animate={{opacity: 1, h: "100%"}} transition={{ duration: 1.5 }} className="font-bold text-3xl"><span className="text-gradient">Hey there</span> 😉</motion.p>
@@ -16,8 +16,8 @@ export function Home(){
       <motion.h1 initial={{x: -100}} animate={{x: 0}}  className="text-4xl font-bold font-mono animate-bounce mt-1">My name's <span className="text-gradient animate-pulse">Daniel</span></motion.h1>
       <motion.h1 initial={{x: 100 }} animate={{x: 0}} className="text-lg text-accent-foreground font-bold">• Fullstack web developer</motion.h1>
       <motion.h1 initial={{y: 150, opacity: 0}} animate={{y: 0, opacity: 1}} transition={{ duration: 1.1 }} className="text-md text-muted-foreground font-bold">Welcome to the simplest portfolio you'd ever see</motion.h1>
-      <motion.div initial={{y: 150, opacity: 0}} animate={{y: 0, opacity: 1}} className="flex gap-2 justify-center items-center">
-        <Switch id="animation" defaultChecked={false} onCheckedChange={(value) => setAnimation(value)}/>
+      <motion.div initial={{y: 150, opacity: 0}} animate={{y: 0, opacity: 1}} className="flex gap-2 justify-start items-center">
+        <Switch id="animation" defaultChecked={animation} onCheckedChange={(value) => setAnimation(value)}/>
         <label htmlFor="animation" className="text-md text-muted-foreground font-bold">Animation</label>
       </motion.div>
       <Button variant="outline" className="backdrop-blur-md w-fit bg-gradient-to-r from-primary to-fuchsia-500" onClick={() => setNav("contact")}>Contact</Button>
