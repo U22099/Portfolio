6d9713aea26966be103f03ba1c24b1e57d62337d
U22099 <nifemiolaniyi4@gmail.com>
2024-12-22 10:27:59 +0100

    update

--- src/components/app/projects-array.js
+++ src/components/app/projects-array.js
@@ -1,13 +1,17 @@
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
+  .matches ?
+  "dark" :
+  "light";
 export const projects = [
   {
-    image: "projects/zephyr.jpg",
+    image: "projects/zephyr-"+systemTheme+".jpg",
     name: "Zephyr",
     description: "Zephyr is a feature-rich chat application built using the Next.js and Firebase.  It offers a comprehensive suite of communication tools, including one-on-one and group chat, video calls, voice calls, status updates, and AI chat interface.  Zephyr is highly customizable, allowing users to personalize their experience",
     github_link: "https://github.com/U22099/Zephyr",
     live_link: "https://zephyr-chat.onrender.com"
   },
   {
-    image: "projects/lumina.jpg",
+    image: "projects/lumina-"+systemTheme+".jpg",
     name: "Lumina",
     description: "Lumina AI is a cutting-edge, multi-modal AI chatbot powered by Gemini API and designed by Swag. Programmed by full-stack web developer Daniel, Lumina combines advanced natural language processing (NLP) with creative and compassionate communication to provide insightful and engaging conversations. Whether you need help with coding, design, brainstorming, or just a friendly chat, Lumina is here to assist you",
     github_link: "https://github.com/U22099/Lumina",
@@ -35,7 +39,7 @@ export const projects = [
     live_link: "https://summify-beryl.vercel.app"
   },
   {
-    image: "projects/code-editor.jpg",
+    image: "projects/code-editor-"+systemTheme+".jpg",
     name: "Web Code Editor",
     description: "A web code editor powered by monaco editor (same editor powering vscode) with code execution feature for languages ranging from javascript, lua, python, typescript and many more",
     live_link: "https://u22099.github.io/WebCodeEditor",
