8552deb0b58fdbb2189b0cf76ca9650669b7a298
U22099 <nifemiolaniyi4@gmail.com>
2025-06-24 16:49:38 +0100

    update

--- src/components/app/projects-array.js
+++ src/components/app/projects-array.js
@@ -4,6 +4,15 @@ const systemTheme = window.matchMedia("(prefers-color-scheme: dark)")
   "light";
 export const projects = [
   {
+    
+    image: "projects/schoolctrl.jpg",
+    name: "School Control",
+    description: "Revolutionize your school management with our comprehensive app designed to connect admins, teachers, parents, and students. This platform offers seamless management of school data, including staff, parents, and students, with full editing capabilities for administrators. Teachers can easily take attendance and access real-time analytics on attendance rates, while students and teachers receive personalized schedule calendars based on the school timetable. Parents can conveniently view the schedules of all their children, ensuring they stay informed. Plus, share important announcements and events school-wide to keep everyone in the loop. Enhance communication, boost efficiency, and elevate your school community with our powerful management tool",
+    live_link: "https://schoolctrl.vercel.app",
+    github_link: "https://github.com/U22099/School-Management-System"
+  
+  },
+  {
     image: "projects/zephyr-"+systemTheme+".jpg",
     name: "Zephyr",
     description: "Zephyr is a feature-rich chat application built using the Next.js and Firebase.  It offers a comprehensive suite of communication tools, including one-on-one and group chat, video calls, voice calls, status updates, and AI chat interface.  Zephyr is highly customizable, allowing users to personalize their experience",
@@ -11,11 +20,11 @@ export const projects = [
     live_link: "https://zephyr-chat.onrender.com"
   },
   {
-    image: "projects/lumina-"+systemTheme+".jpg",
-    name: "Lumina",
-    description: "Lumina AI is a cutting-edge, multi-modal AI chatbot powered by Gemini API and designed by Swag. Programmed by full-stack web developer Daniel, Lumina combines advanced natural language processing (NLP) with creative and compassionate communication to provide insightful and engaging conversations. Whether you need help with coding, design, brainstorming, or just a friendly chat, Lumina is here to assist you",
-    github_link: "https://github.com/U22099/Lumina",
-    live_link: "https://u22099.github.io/Lumina"
+    image: "projects/nex-ai-"+systemTheme+".jpg",
+    name: "NexAI",
+    description: "NexAI is a sophisticated chatbot powered by Google's Gemini model, designed to elevate your user experience through precise and contextually aware responses. Whether you need assistance with inquiries, task management, or professional support, NexAI is here to assist with adaptive learning and a seamless interface",
+    github_link: "https://github.com/U22099/NexAI",
+    live_link: "https://nex-aix.vercel.app"
   },
   {
     image: "projects/melodia-api.jpg",
